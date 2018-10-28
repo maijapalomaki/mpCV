e1fcf6261c5e5cb7bb5d914b052c582df11470e5
maijapalomaki <maija.palomaki@student.laurea.fi>
2018-10-28 20:22:20 +0200

    Initial Commit

---
+++ src/pages/contact/contact.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/**
+ * Generated class for the ContactPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+
+@Component({
+  selector: 'page-contact',
+  templateUrl: 'contact.html',
+})
+export class ContactPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
---
+++ src/pages/list/list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-list',
+  templateUrl: 'list.html'
+})
+
+export class ListPage { constructor(public navCtrl: NavController) {
+}
+}
